befd62e7385d1543c045155edd2811bccb3017e8
s-ahmed-naim <naim.ahmed035@gmail.com>
2021-10-25 19:17:41 +0600

    Authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TodoModule,
+    AuthModule,
     MongooseModule.forRoot('mongodb://localhost/nest-todo'),
   ],
   controllers: [AppController],
